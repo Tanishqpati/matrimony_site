92e89c29c1ad60bc085b78565cf4f8f080263af8
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-06-16 16:05:07 +0530

    chore: Update app.js with CORS configuration and middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`)
+// })
+
+export { app }
