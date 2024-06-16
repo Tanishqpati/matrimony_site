2692cff8673477dcb5d2a7586acbabf0b0239140
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-06-16 16:05:31 +0530

    chore: Update server startup code to include error handling for MongoDB connection

--- server/src/index.js
+++ server/src/index.js
@@ -4,3 +4,11 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: './env' })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGO_DB Connection Failed", error)
+})
